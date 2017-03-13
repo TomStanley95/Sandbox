88d8c2b30f9a614a4ff46b0deec5a05ca4c66b3a
TomStanley95 <tom.stanley@my.jcu.edu.au>
2017-03-13 15:25:11 +1000

    add name printing program

--- oddName.py
+++ oddName.py
@@ -1,2 +1,11 @@
 __author__ = 'Tom Stanley'
 """Tom Stanley"""
+finished = False
+user_name = str(input("Please enter your name:").strip(" "))
+name_length = len(user_name)
+while user_name == "":
+        user_name = str(input("Your name cant be nothing.Please enter your name:").strip())
+for i in range(1, name_length, 2):
+    print(user_name[i])
+
+
